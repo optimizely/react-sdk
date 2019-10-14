95c14e9703883bce89a609fcc36ffefdb557509c
Colin Hemphill <hemphill.colin@gmail.com>
2019-10-14 11:27:52 -0500

    Export context consumer/provider pair

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+export { OptimizelyContextConsumer, OptimizelyContextProvider } from './Context'
 export { OptimizelyProvider } from './Provider'
 export { OptimizelyFeature } from './Feature'
 export { withOptimizely, WithOptimizelyProps, WithoutOptimizelyProps } from './withOptimizely'
