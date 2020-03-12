b60b015fb3aeae74f454f4ccf22d84a0121e951c
James Fox <james@optimizely.com>
2020-03-12 15:47:11 -0700

    chore (ESLint): Add eslint + typescript + prettier support (#31)
    ## Summary
    
    Setup ESLint w/Typescript & React support
    
    Autolint 2 files just to make sure it works.
    
    Once the core code is approved, I'll autolint the entire repo in another PR.
    
    ### Approach
    
    I used the [Prettier.io recommended setup](https://prettier.io/docs/en/integrating-with-linters.html#recommended-configuration) with ESLint as well as [this guide](https://www.robertcooper.me/using-eslint-and-prettier-in-a-typescript-project) to help integrate Typescript.
    
    The `.prettierrc` file was copied directly over from the javascript-sdk.
    
    A few rules (3) were turned off so that after this PR, `eslint --fix` leaves us with no errors.
    
    We can discuss after that if we want to turn those rules back on.

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2015,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    mocha: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+
+    // Prettier should always go last so it can trump any rules above
+    'plugin:prettier/recommended',
+    'prettier/react',
+  ],
+  rules: {
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+  },
+};
