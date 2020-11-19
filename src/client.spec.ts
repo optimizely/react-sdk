a1689932230abe39b45508fef3a5aae9d3b343db
Polina Nguen <polina.nguen@optimizely.com>
2020-11-19 14:53:15 -0800

    Prepare for 2.4.1 release

--- src/client.spec.ts
+++ src/client.spec.ts
@@ -91,7 +91,7 @@ describe('ReactSDKClient', () => {
     expect(createInstanceSpy).toBeCalledWith({
       ...config,
       clientEngine: 'react-sdk',
-      clientVersion: '2.4.0',
+      clientVersion: '2.4.1',
     });
   });
 
