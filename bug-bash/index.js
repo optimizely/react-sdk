6d67dfe73af9b2a4299d05836dee3270d7905ac3
Mike Chu <michael.chu@optimizely.com>
2023-08-04 20:36:22 +0000

    Change back to scripty .js

--- bug-bash/index.js
+++ bug-bash/index.js
@@ -1,10 +1,14 @@
-import dotenv from 'dotenv';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenv = require('dotenv');
 
 dotenv.config();
 
-const scenario: string = process.argv[2];
+const scenario = process.argv[2];
+const sdkKey = process.env.SDK_KEY;
 
-switch(scenario) {
+console.log('SDK Key: ', sdkKey);
+
+switch (scenario) {
   case 'scenario1':
     console.log('Running setup for scenario 1...');
     // Code to set up scenario 1
