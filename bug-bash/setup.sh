cca10847a11b637f1b330508b618b74d5ee0cfe3
Mike Chu <michael.chu@optimizely.com>
2023-08-04 20:23:05 +0000

    Enhance setup.sh

--- bug-bash/setup.sh
+++ bug-bash/setup.sh
@@ -3,8 +3,26 @@
 echo "Welcome to the Bug Bash 🐞"
 echo "Setting up for the bug bash..."
 
-# Prompt for SDK key
-read -p "Please enter your SDK key: " sdk_key
+# Install dotenv via NPM instead of yarn so as not to save to package.json 
+npm install --no-save --legacy-peer-deps --silent dotenv 
+# Revert changes to yarn.lock file
+git checkout -- yarn.lock
 
-# Create .env file or append to existing one
-echo "SDK_KEY=$sdk_key" >> .env
+# Prompt for SDK key
+echo
+echo "\033[1;38;2;0;55;255mPlease enter your SDK key: \033[0m\c"
+read sdk_key
+
+# Check if .env file contains SDK_KEY entry
+if grep -q "SDK_KEY" .env; then
+  # If it does, replace the existing entry
+  sed -i "s/^SDK_KEY=.*/SDK_KEY=$sdk_key/" .env
+else
+  # If it doesn't, append the new entry
+  echo "SDK_KEY=$sdk_key" >> .env
+fi
+
+echo
+echo "\033[1;38;2;59;224;129mReady\033[0m."
+echo "Please run \"npm run bug-bash -- <your-scenario>\" (note the spaces before & after --)"
+echo
