d4f75a631f6850ac62fc764f555ff60cc4d2168d
Mike Chu <michael.chu@optimizely.com>
2023-08-15 21:25:43 +0000

    WIP testing & refactoring tests

--- bug-bash/app/src/App.tsx
+++ bug-bash/app/src/App.tsx
@@ -2,93 +2,94 @@ import React, { useEffect, useState } from 'react';
 import { createInstance, OptimizelyProvider, useDecision } from '@optimizely/react-sdk';
 import { OptimizelyReturnType } from './App.types';
 
-import { OdpSegmentManager } from '@optimizely/optimizely-sdk/lib/core/odp/odp_segment_manager';
-import { OptimizelySegmentOption } from '@optimizely/optimizely-sdk/lib/core/odp/optimizely_segment_option';
+import { OptimizelySegmentOption } from '@optimizely/optimizely-sdk';
 
 const sdkKey = import.meta.env.VITE_SDK_KEY as string; // update in .env.local file
+const logLevel = 'info'; // adjust as you needed; 'debug' needed later
 
+const bugBashLog = (message: string) => {
+  console.log(`%c🐝[BUG BASH] - ${message}`, "color: orange; font-size: 20px;");
+};
 export const App: React.FC = () => {
   const [featureKey, setFeatureKey] = useState<string>('some_key');
-  const [isSegmentsFetched, setIsSegmentsFetched] = useState<boolean | null>(null);
   const [readyResult, setReadyResult] = useState<OptimizelyReturnType>();
   const [enableDecision, setEnableDecision] = useState<boolean>(false);
 
-  /* ⬇️ Tests are below this line ⬇️ */
+  const optimizelyClient = createInstance({ sdkKey, logLevel });
 
-  // 1. console should show two qualified segments and a viud
-  const optimizelyClient = createInstance({ sdkKey });
-  const [userId] = useState<string>('matjaz-user-1');
-  const prepareClient = () => {
-    optimizelyClient.onReady().then(async (res: any) => {
-      setReadyResult(res);
-      setIsSegmentsFetched(true);
-    });
+  const handleReadyResult = async (res: OptimizelyReturnType) => {
+    const userContext = optimizelyClient?.userContext;
+    if (userContext) {
+      bugBashLog(`_qualifiedSegments: ${optimizelyClient['userContext']['_qualifiedSegments'] as string}`);
+    } else {
+      bugBashLog(`userContext is null`);
+    }
+
+    if (userId) {
+      bugBashLog(`userId: ${userId}`);
+    }
+
+    const currentVuid = localStorage.getItem('optimizely-vuid');
+    if (currentVuid) {
+      bugBashLog(`vuid: ${currentVuid}`);
+    }
+    
+    setReadyResult(res);
   };
 
-  // 2. console should show three qualified segments and the same viud
-  // const optimizelyClient = createInstance({ sdkKey });
+  /* ⬇️ Tests are below this line ⬇️ */
+
+  /* 1. console [BUG BASH 🐝] should show two qualified segments and a viud */
+  // const [userId] = useState<string>('matjaz-user-1');
+  // const prepareClient = () => {
+  //   optimizelyClient.onReady().then(handleReadyResult);
+  // };
+
+  /* 2. console should now show three qualified segments and the same viud */
   // const [userId] = useState<string>('matjaz-user-2');
   // const prepareClient = () => {
-  //   optimizelyClient.onReady().then(async (res: any) => {
-  //     setReadyResult(res);
-  //     setIsSegmentsFetched(true);
-  //   });
+  //   optimizelyClient.onReady().then(handleReadyResult);
   // };
 
-  // 3. console should show no qualified segments and the same viud
-  // const optimizelyClient = createInstance({ sdkKey });
+  /* 3. console should show now show no qualified segments and the same vuid */
   // const [userId] = useState<string>('matjaz-user-3');
   // const prepareClient = () => {
-  //   optimizelyClient.onReady().then(async (res: any) => {
-  //     setReadyResult(res);
-  //     setIsSegmentsFetched(true);
-  //   });
+  //   optimizelyClient.onReady().then(handleReadyResult);
   // };
 
-  // 4. console should show no qualified segments and the same viud
+  /* 4. console should show no qualified segments and the same vuid */
   // const [userId] = useState<null>(null);
-  // const optimizelyClient = createInstance({ sdkKey });
   // const prepareClient = () => {
-  //   optimizelyClient.onReady().then(async (res: any) => {
-  //     setReadyResult(res);
-  //     setIsSegmentsFetched(true);
-  //   });
+  //   optimizelyClient.onReady().then(handleReadyResult);
   // };
 
-  // 5. the network tab zaious call should be sending the vuid_xxxxx as the vuid and there shouldnt be a fs_userid sent
+  /* 5. the network tab has graphql calls 
+     click the latest one and inspect the Payload 
+     there should be a `vuid: vuid_xxxxx` as the vuid and there should not be a fs_userid sent */
   // const [userId] = useState<string>('vuid_overridden');
-  // const optimizelyClient = createInstance({ sdkKey });
   // const prepareClient = () => {
-  //   optimizelyClient.onReady().then(async (res: any) => {
-  //     setReadyResult(res);
-  //     setIsSegmentsFetched(true);
-  //   });
+  //   optimizelyClient.onReady().then(handleReadyResult);
   // };
 
-  // 6. the network tab should show 2 graphql calls
-  // const [userId] = useState<string>('matjaz-user-3');
-  // const optimizelyClient = createInstance({ sdkKey });
-  // const prepareClient = () => {
-  //   optimizelyClient.onReady().then(async (res: any) => {
-  //     setReadyResult(res);
-  //     setIsSegmentsFetched(true);
-  //   });
-  // };
-  // useEffect(() => {
-  //   const callSegments = async () => {
-  //     if (readyResult?.success) {
-  //       await optimizelyClient.fetchQualifiedSegments([OptimizelySegmentOption.RESET_CACHE]);
-  //       await optimizelyClient.fetchQualifiedSegments();
-  //     }
-  //   };
-  //   callSegments();
-  // }, [readyResult?.success]);
+  /* 6. the network tab should show 2 graphql calls */
+  const [userId] = useState<string>('matjaz-user-3');
+  const prepareClient = () => {
+    optimizelyClient.onReady().then(handleReadyResult);
+  };
+  useEffect(() => {
+    const callSegments = async () => {
+      if (readyResult?.success) {
+        await optimizelyClient.fetchQualifiedSegments([OptimizelySegmentOption.RESET_CACHE]);
+        await optimizelyClient.fetchQualifiedSegments();
+      }
+    };
+    callSegments();
+  }, [readyResult?.success]);
 
   // 7. the network tab should show 2 graphql calls
   // const [userId] = useState<string>('matjaz-user-3');
-  // const optimizelyClient = createInstance({ sdkKey });
   // const prepareClient = () => {
-  //   optimizelyClient.onReady().then(async (res: any) => {
+  //   optimizelyClient.onReady().then(async (res: OptimizelyReturnType) => {
   //     setReadyResult(res);
   //     setIsSegmentsFetched(true);
   //   });
@@ -106,8 +107,7 @@ export const App: React.FC = () => {
   // 8. there should be an error for the first call to fetchQualifiedSegments the second call should work fine
   // const [userId] = useState<string>('random-user-id');
   // const prepareClient = () => {
-  //   console.log('optimizelyClient');
-  //   optimizelyClient.onReady().then(async (res: any) => {
+  //   optimizelyClient.onReady().then(async (res: OptimizelyReturnType) => {
   //     setReadyResult(res);
   //     setIsSegmentsFetched(true);
   //     await optimizelyClient.fetchQualifiedSegments([OptimizelySegmentOption.IGNORE_CACHE]);
@@ -115,10 +115,9 @@ export const App: React.FC = () => {
   // };
 
   // 9. fetch should return error in the console, for segment fetch
-  // const optimizelyClient = createInstance({ sdkKey });
   // const [userId] = useState<string>('matjaz-user-2');
   // const prepareClient = () => {
-  //   optimizelyClient.onReady().then(async (res: any) => {
+  //   optimizelyClient.onReady().then(async (res: OptimizelyReturnType) => {
   //     setReadyResult(res);
   //     setIsSegmentsFetched(true);
   //     await optimizelyClient.fetchQualifiedSegments([OptimizelySegmentOption.IGNORE_CACHE]);
@@ -127,7 +126,7 @@ export const App: React.FC = () => {
 
   // 10. odp network error
   // const optimizelyClient = createInstance({
-  //   logLevel: 'debug',
+  //   logLevel,
   //   sdkKey,
   //   datafileOptions: {
   //     urlTemplate: 'https://httpstat.us/521?sdkKey=%s',
@@ -135,7 +134,7 @@ export const App: React.FC = () => {
   // });
   // const [userId] = useState<string>('matjaz-user-2');
   // const prepareClient = () => {
-  //   optimizelyClient.onReady().then(async (res: any) => {
+  //   optimizelyClient.onReady().then(async (res: OptimizelyReturnType) => {
   //     setReadyResult(res);
   //     setIsSegmentsFetched(true);
   //     await optimizelyClient.fetchQualifiedSegments([OptimizelySegmentOption.IGNORE_CACHE]);
@@ -150,9 +149,8 @@ export const App: React.FC = () => {
   //   },
   // });
   // const [userId] = useState<string>('matjaz-user-2');
-
   // const prepareClient = () => {
-  //   optimizelyClient.onReady().then(async (res: any) => {
+  //   optimizelyClient.onReady().then(async (res: OptimizelyReturnType) => {
   //     setReadyResult(res);
   //     setIsSegmentsFetched(true);
   //     await optimizelyClient.fetchQualifiedSegments([OptimizelySegmentOption.IGNORE_CACHE]);
@@ -168,7 +166,7 @@ export const App: React.FC = () => {
   // });
   // const [userId] = useState<string>('matjaz-user-2');
   // const prepareClient = () => {
-  //   optimizelyClient.onReady().then(async (res: any) => {
+  //   optimizelyClient.onReady().then(async (res: OptimizelyReturnType) => {
   //     setReadyResult(res);
   //     setIsSegmentsFetched(true);
   //     await optimizelyClient.fetchQualifiedSegments([OptimizelySegmentOption.IGNORE_CACHE]);
@@ -178,12 +176,9 @@ export const App: React.FC = () => {
 
   // 13. call decide for a segment user is not a part of hence user should not qualify,
   // later make the user part of the segment and call decide again to check if user qualifies
-  // const optimizelyClient = createInstance({
-  //   sdkKey,
-  // });
   // const [userId] = useState<string>('matjaz-user-2');
   // const prepareClient = () => {
-  //   optimizelyClient.onReady().then(async (res: any) => {
+  //   optimizelyClient.onReady().then(async (res: OptimizelyReturnType) => {
   //     setReadyResult(res);
   //     setFeatureKey('test_feature_1')
   //     setIsSegmentsFetched(true);
@@ -202,9 +197,6 @@ export const App: React.FC = () => {
           <h1>Bug Bash Output</h1>
           <h2>Please open your browser's "Developer tools" (Ctrl-Shift-I) for Console output</h2>
           <pre>
-            <p>{`Is segments fetched for user '${userId}': ${isSegmentsFetched ? 'Yes' : 'No'} `}</p>
-            <p>UserID: {optimizelyClient.user.id === null ? 'null' : optimizelyClient.user.id}</p>
-            <p>VUID: {localStorage.getItem('optimizely-vuid')}</p>
             {enableDecision && featureKey && <Decision userId={userId} featureKey={featureKey} />}
           </pre>
         </>
@@ -237,7 +229,9 @@ function Decision({ userId, featureKey }: { userId: string; featureKey: string }
 
   return (
     <p>
-      {`Flag ${ decision.enabled ? 'on' : 'off'}. User number ${userId} saw flag variation: ${variationKey} and got products sorted by: ${sortMethod} config variable as part of flag rule: ${
+      {`Flag ${
+        decision.enabled ? 'on' : 'off'
+      }. User number ${userId} saw flag variation: ${variationKey} and got products sorted by: ${sortMethod} config variable as part of flag rule: ${
         decision.ruleKey
       }`}
     </p>
