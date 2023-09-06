3dae9cfdf16960e93efd90c04a9b612f84e591f1
Mike Chu <michael.chu@optimizely.com>
2023-09-06 19:36:29 +0000

    Remove optimizeDeps from viteconfig

--- bug-bash/app/vite.config.ts
+++ bug-bash/app/vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@optimizely/optimizely-sdk'],
-  },
   define: {
     'process.env': process.env
   }
