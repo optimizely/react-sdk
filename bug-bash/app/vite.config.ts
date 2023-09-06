4a49829f077d9825554c5f4c6cde82c1900e265d
Mike Chu <michael.chu@optimizely.com>
2023-09-06 19:04:35 +0000

    Explicitly optimizeDep-endcy JS SDK

--- bug-bash/app/vite.config.ts
+++ bug-bash/app/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@optimizely/optimizely-sdk'],
+  },
   define: {
     'process.env': process.env
   }
