38d21fc2abc5b4256b97c70756c3a0756e730a5e
Mike Chu <104384559+mikechu-optimizely@users.noreply.github.com>
2024-01-24 14:58:16 -0500

    [FSSDK-8721] chore: prep release 3.0.0 (#231)
    * build: bump SDK version
    
    * docs: update changelog
    
    * docs: changelog corrections
    
    * docs: add bug fixes + formatting

--- src/client.spec.ts
+++ src/client.spec.ts
@@ -123,7 +123,7 @@ describe('ReactSDKClient', () => {
     expect(createInstanceSpy).toBeCalledWith({
       ...config,
       clientEngine: 'react-sdk',
-      clientVersion: '3.0.0-beta2',
+      clientVersion: '3.0.0',
     });
   });
 
