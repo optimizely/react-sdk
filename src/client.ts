c16ea02cb423c78b9de09b2e78e0d40b590ee38a
Md Junaed Hossain <169046794+junaed-optimizely@users.noreply.github.com>
2024-08-15 23:14:38 +0600

    [3.2.1] Prep for release (#275)
    * [3.2.1] Prep for release
    
    * [3.2.1] PR ref update

--- src/client.ts
+++ src/client.ts
@@ -47,7 +47,7 @@ export interface OnReadyResult extends ResolveResult {
 }
 
 const REACT_SDK_CLIENT_ENGINE = 'react-sdk';
-const REACT_SDK_CLIENT_VERSION = '3.2.0';
+const REACT_SDK_CLIENT_VERSION = '3.2.1';
 
 export const DefaultUser: UserInfo = {
   id: null,
