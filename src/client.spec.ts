7e9bda9332980206293567a9acb84c434ca3c848
Md Junaed Hossain <169046794+junaed-optimizely@users.noreply.github.com>
2024-08-22 18:39:49 +0600

    [FSSDK-10554] prep for release (#277)

--- src/client.spec.ts
+++ src/client.spec.ts
@@ -128,7 +128,7 @@ describe('ReactSDKClient', () => {
     expect(createInstanceSpy).toBeCalledWith({
       ...config,
       clientEngine: 'react-sdk',
-      clientVersion: '3.2.1',
+      clientVersion: '3.2.2',
     });
   });
 
