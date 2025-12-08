8bec1bc69decf64f2da5ebd522a10411f152d4cb
Md Junaed Hossain <169046794+junaed-optimizely@users.noreply.github.com>
2025-12-08 21:28:14 +0600

    release 3.3.1 (#310)
    Confirmed passing prisma for this PR

--- src/client.spec.ts
+++ src/client.spec.ts
@@ -178,7 +178,7 @@ describe('ReactSDKClient', () => {
       expect(createInstanceSpy).toHaveBeenCalledWith({
         ...config,
         clientEngine: 'react-sdk',
-        clientVersion: '3.3.0',
+        clientVersion: '3.3.1',
       });
     });
 
