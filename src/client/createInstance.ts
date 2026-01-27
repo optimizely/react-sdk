53d29e7081863c78ff45f003f1c550b76224090e
Md Junaed Hossain <169046794+junaed-optimizely@users.noreply.github.com>
2026-01-27 20:00:22 +0600

    [FSSDK-12244] React client config exposure (#315)

---
+++ src/client/createInstance.ts
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2026, Optimizely
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { createInstance as jsCreateInstance } from '@optimizely/optimizely-sdk';
+import type { Config, Client } from '@optimizely/optimizely-sdk';
+
+const CLIENT_ENGINE = 'react-sdk';
+const CLIENT_VERSION = '4.0.0';
+
+/**
+ * Creates an Optimizely client instance for use with React SDK.
+ *
+ * @param config - Configuration object for the Optimizely client
+ * @returns An OptimizelyClient instance
+ */
+export function createInstance(config: Config): Client {
+  return jsCreateInstance({
+    ...config,
+    clientEngine: CLIENT_ENGINE,
+    clientVersion: CLIENT_VERSION,
+  });
+}
